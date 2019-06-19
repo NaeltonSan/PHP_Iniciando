937b7fba33c13fbb5a1e3664449bb1383b674fb1
Naelton Santos <naeon02@gmail.com>
2019-06-18 23:32:03 -0300

    ordem de precedencia

---
+++ 02-operadores-aritmeticos.php
@@ -0,0 +1,13 @@
+<?php
+
+    $x = 1 + 2 * 3;
+    $y = (1 + 2) * 3;
+    echo $x; //7
+    echo "<br>";
+    echo $y; //9
+    echo "<br>";
+    echo 2**4;  //potencia
+    echo "<br>";
+    echo 5 % 2 ; //resto da divisao
+
+?>
