1cad851262a230781f9a17767e18fa87418515a5
Naelton Santos <naeon02@gmail.com>
2019-06-20 16:23:30 -0300

    php-html

---
+++ 09-php-html.php
@@ -0,0 +1,15 @@
+<?php 
+    $nome = "Luiz Carlos";
+    $html = "<h1>Iniciando com PHP</h1>";
+?>
+<html>
+    <head></head>
+    <body>
+        <?php 
+                echo 5 + 3; 
+                echo "<br>";
+                echo $nome;
+                echo $html; 
+        ?>
+    </body>
+</html>
