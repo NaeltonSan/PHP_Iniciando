52375e26ee10d5265aac226e3b7e19c8fdec8272
Naelton Santos <naeon02@gmail.com>
2019-06-20 15:15:13 -0300

    String

---
+++ 04-string.php
@@ -0,0 +1,22 @@
+<?php
+        echo "Qualquer coisa...";
+        $variavel = "School of Net";
+        echo $variavel;
+        $string1 = "School of Net";
+        echo "<br>";
+        $string2 = "Iniciando com PHP"; 
+        echo $string1 . $string2;
+        echo "<br>";
+        echo "Luiz" . " Carlos";
+        echo "<br>";
+        echo $string1 .' '. $string2;
+        $string3 = $string1 .' '. $string2;
+        $string3 = "School of Net Inciando com PHP";
+        $string4 = "$string1      $string2";
+        echo "<br>";
+        echo $string4;
+        $string5 = '$string1 $string2';
+        echo "<br>";
+        echo $string5;
+      
+?>
