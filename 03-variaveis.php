39025c1379826ad37ffdf5f4aac12b355a04e086
Naelton Santos <naeon02@gmail.com>
2019-06-19 22:23:15 -0300

    variaveis

---
+++ 03-variaveis.php
@@ -0,0 +1,25 @@
+<?php
+        $nome = "School of Net"; 
+        echo $nome;
+        $nome = 'Iniciando com PHP';
+        echo "<br>";
+        echo $nome;
+        $nome = 5000;
+        echo "<br>";
+        echo $nome;
+        $numero = 5000;
+        echo "<br>";
+        echo $numero;
+        $NOME = 'school of net';
+        $NOME234 = 'school of net outra variavel';
+        $nome2213nome = 'asdfasdf';
+        $primeiro_nome = 'luiz';
+        $primeiroNome = 'luiz';
+        $variavelTrue = true; 
+        $variavelFalse = false; 
+        echo "<br>";
+        echo ($variavelTrue);
+        echo "<br>";
+        echo var_dump($variavelFalse);
+        
+?>
